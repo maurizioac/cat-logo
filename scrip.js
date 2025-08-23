6c9beaef22865039165089c5aa3092a949325486
Maurizio <maurizioac6@gmail.com>
2025-08-23 10:30:45 -0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const detalleButtons = document.querySelectorAll(".detalle-btn");
 
-  detalleButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const ciudadItem = button.closest(".ciudades-item");
-      if (ciudadItem) {
-        ciudadItem.classList.toggle("active");
-      }
-    });
-  });
-});
